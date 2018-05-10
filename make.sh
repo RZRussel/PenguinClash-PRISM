b97e1218c17e4da6d8416d17d156e6bde8d5de7d
RZRussel <emkil.minor@gmail.com>
2018-05-10 11:02:55 +0300

    initial commit

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+i#!/bin/sh
 
 echo "Building model..."
 python ABSGenerator/modelgen.py -t resources/template.prism -s resources/settings.yaml -g PGGenerator.py -o resources/model.prism
 
 echo "Did start verification: $(date)"
-prism -javamaxmem 32g -cuddmaxmem 32g resources/model.prism resources/properties.prop -m -v
+prism -javamaxmem 32g -cuddmaxmem 32g resources/model.prism resources/properties.prop -m -extraddinfo -extrareachinfo
 echo "Did finish verification: $(date)"
